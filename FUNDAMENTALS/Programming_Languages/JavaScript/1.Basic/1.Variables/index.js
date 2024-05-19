b9261904df01edd7b5f42ad06babca492c9ecccc
Ifham <ifhammohamed1111@gmail.com>
2024-05-19 20:23:14 +0530

    chore: Add JavaScript Variables lesson and code examples

---
+++ FUNDAMENTALS/Programming_Languages/JavaScript/1.Basic/1.Variables/index.js
@@ -0,0 +1,26 @@
+let age = 23;
+console.log("🚀 ~ age:", age);
+
+let year = 2024;
+console.log("🚀 ~ year:", year);
+
+age = 30;
+console.log("🚀 ~ age:", age);
+
+const points = 100;
+console.log("🚀 ~ points:", points);
+
+// points = 50; // Error
+// console.log("🚀 ~ points    :", points);
+// Uncaught TypeError: Assignment to constant variable.
+
+var score = 75;
+console.log("🚀 ~ score:", score);
+
+score = 45;
+console.log("🚀 ~ score:", score);
+// Older way of declaring variables
+
+// let score = 100; // Error
+// console.log("🚀 ~ score:", score);
+// Uncaught SyntaxError: Identifier 'score' has already been declared (at index.js:22:5)
